c8221dca54e10c9e5493af635fa9407dd3cef491
Berkay-Disli <berkaydisli.work@gmail.com>
2022-10-04 14:53:38 +0300

    Initial Commit

---
+++ MorphingShapes-Practice/MorphingShapes_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorphingShapes_PracticeApp.swift
+//  MorphingShapes-Practice
+//
+//  Created by Berkay Disli on 4.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MorphingShapes_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
